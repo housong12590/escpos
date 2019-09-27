2efd6a840eb5a931527534f1def77c18d9c170b5
housong <304536797@qq.com>
2019-09-27 18:03:32 +0800

    修改打印BUG

--- src/main/java/com/ciin/pos/Test.java
+++ src/main/java/com/ciin/pos/Test.java
@@ -1,8 +1,9 @@
 package com.ciin.pos;
 
-import gnu.io.*;
-
 import java.io.IOException;
+
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
 
 public class Test {
 
@@ -13,13 +14,14 @@ public class Test {
     public static void main(String[] args) throws InterruptedException, IOException, NoSuchPortException, PortInUseException {
 //        Printer printer = new SerialPortPrinter(DeviceFactory.getDefault(), "COM4", 38400);
 ////        Printer printer = new DrivePrinter(DeviceFactory.getDefault(), "\\\\DESKTOP-GSJUGH8\\GP-C80300 Series");
-//        for (int i = 0; i < 1; i++) {
+//        for (int i = 0; i < 10; i++) {
 //            String read = FileUtils.fileRead("D:\\work\\java\\printer\\printer_client\\src\\main\\resources\\template\\test_print.xml");
 //            PrintTask printTask = new PrintTask(new Template(read));
 //            printer.print(printTask);
+//            Utils.sleep(90 * 60 * 1000);
 //        }
-//        Utils.sleep(10 * 1000);
-//        printer.close();
+
+
+
     }
-    //mvn install:install-file -Dfile=E:\code\java\escpos\libs\android.jar -DgroupId=com.android -DartifactId=android -Dversion=1.0.0 -Dpackaging=jar
 }
