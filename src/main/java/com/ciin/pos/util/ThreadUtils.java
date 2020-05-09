9a309395143b529eec532769e86e6fd866ce89a8
hous <304536797@qq.com>
2020-05-09 15:47:21 +0800

    更新代码

--- src/main/java/com/ciin/pos/util/ThreadUtils.java
+++ src/main/java/com/ciin/pos/util/ThreadUtils.java
@@ -1,10 +1,20 @@
 package com.ciin.pos.util;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadUtils {
 
     public static void sleep(long millis) {
         try {
             Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sleep(int value, TimeUnit unit) {
+        try {
+            unit.sleep(value);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
